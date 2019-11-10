44bc7d75a0c821c1678811fad75256511aef3249
Emir Gökdemir <44062527+gokdeemir@users.noreply.github.com>
2019-11-11 01:40:02 +0300

    RegisterDto is created

---
+++ purposeful-community-cloud/src/main/java/com/evteam/purposefulcommunitycloud/model/dto/RegisterDto.java
@@ -0,0 +1,25 @@
+package com.evteam.purposefulcommunitycloud.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+/**
+ * Created by Emir Gökdemir
+ * on 11 Kas 2019
+ */
+@Getter
+@Setter
+public class RegisterDto {
+
+    private String email;
+
+    private String studentId;
+
+    private String name;
+
+    private String surname;
+
+    @Length(min=10,max=13)
+    private String phone;
+}
