cbb4e622937557ebf5e69769848998aaf7141f4b
Emir Gökdemir <44062527+gokdeemir@users.noreply.github.com>
2019-11-10 23:39:46 +0300

    Cors config is created

---
+++ purposeful-community-cloud/src/main/java/com/evteam/purposefulcommunitycloud/configuration/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.evteam.purposefulcommunitycloud.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by Emir Gökdemir
+ * on 13 Eki 2019
+ */
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+                .allowCredentials(true);
+    }
+}
