68cc71e0b721f929b1a4bb977d623b9562ed53fe
Emir Gökdemir <44062527+gokdeemir@users.noreply.github.com>
2019-11-11 00:47:20 +0300

    Jwt Token mechanism is injected

---
+++ purposeful-community-cloud/src/main/java/com/evteam/purposefulcommunitycloud/security/JwtGenerator.java
@@ -0,0 +1,31 @@
+package com.evteam.purposefulcommunitycloud.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Component
+public class JwtGenerator {
+
+    private static final long JWT_TOKEN_VALIDITY = 10 * 60 * 60; // 10 hours
+
+    @Value("${jwt.secret}") private String secret;
+
+    // Generates a token with the given user's id and current time
+    public String generateToken(UUID id) {
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(id.toString())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
+                .signWith(SignatureAlgorithm.HS512, secret).compact();
+    }
+
+}
---
+++ purposeful-community-cloud/src/main/java/com/evteam/purposefulcommunitycloud/security/JwtResolver.java
@@ -0,0 +1,44 @@
+package com.evteam.purposefulcommunitycloud.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.UUID;
+import java.util.function.Function;
+
+import static com.evteam.purposefulcommunitycloud.constant.ErrorConstants.INVALID_TOKEN;
+
+@Component
+public class JwtResolver {
+
+    @Value("${jwt.secret}") private String secret;
+
+    // Extracts the username(email) from the given token
+    public UUID getIdFromToken(String token) {
+        String idString;
+        try{
+         idString = getClaimFromToken(token, Claims::getSubject);
+        } catch (Exception e){
+            throw new RuntimeException(INVALID_TOKEN);
+        }
+        return UUID.fromString(idString);
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    private <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+
+}
