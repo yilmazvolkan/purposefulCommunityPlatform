63b7e95a8917c564e041c88ac7b6beacf3cbb877
Emir Gökdemir <44062527+gokdeemir@users.noreply.github.com>
2019-11-10 15:26:53 +0300

    cloud initilized

---
+++ purposeful-community-cloud/src/main/java/com/evteam/purposefulcommunitycloud/PurposefulCommunityCloudApplication.java
@@ -0,0 +1,13 @@
+package com.evteam.purposefulcommunitycloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurposefulCommunityCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurposefulCommunityCloudApplication.class, args);
+	}
+
+}
---
+++ purposeful-community-cloud/src/test/java/com/evteam/purposefulcommunitycloud/PurposefulCommunityCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evteam.purposefulcommunitycloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurposefulCommunityCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
