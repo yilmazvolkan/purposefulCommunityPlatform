3bbc415fb8783e99d158bfc9627670a21d32e676
Emir Gökdemir <44062527+gokdeemir@users.noreply.github.com>
2019-11-14 00:41:32 +0300

    Send mail is added Send activation token is added ResetPassword Request is added Confirmation reset password is added

---
+++ purposeful-community-cloud/src/main/java/com/evteam/purposefulcommunitycloud/constant/ResponseConstants.java
@@ -0,0 +1,11 @@
+package com.evteam.purposefulcommunitycloud.constant;
+
+/**
+ * Created by Emir Gökdemir
+ * on 12 Kas 2019
+ */
+
+public class ResponseConstants {
+    public static final String MAIL_SENT_TO_THAT_ADDRESS="Mail sent that address: ";
+    public static final String USER_IS_CONFIRMED="User is confirmed. Mail adress is: ";
+}
--- purposeful-community-cloud/src/main/java/com/evteam/purposefulcommunitycloud/service/UserService.java
+++ purposeful-community-cloud/src/main/java/com/evteam/purposefulcommunitycloud/service/UserService.java
@@ -10,10 +10,25 @@ import com.evteam.purposefulcommunitycloud.model.resource.LoginResource;
 import com.evteam.purposefulcommunitycloud.model.resource.UserResource;
 import com.evteam.purposefulcommunitycloud.repository.UserRepository;
 import com.evteam.purposefulcommunitycloud.security.JwtGenerator;
+import com.evteam.purposefulcommunitycloud.security.JwtResolver;
+import org.omg.PortableInterceptor.USER_EXCEPTION;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+
 import javax.transaction.Transactional;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.evteam.purposefulcommunitycloud.constant.ErrorConstants.USER_NOT_EXIST;
+import static com.evteam.purposefulcommunitycloud.constant.MailConstants.*;
 
 /**
  * Created by Emir Gökdemir
@@ -34,19 +49,98 @@ public class UserService {
     @Autowired
     private JwtGenerator jwtGenerator;
 
+    @Autowired
+    private JwtResolver jwtResolver;
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
     @Transactional
-    public UserResource register(RegisterDto registerDto){
-        return registerMapper.toResource(repository.saveAndFlush(registerMapper.toEntity(registerDto)));
+    public UserResource register(RegisterDto registerDto) {
+        User user = repository.saveAndFlush(registerMapper.toEntity(registerDto));
+        sendActivationToken(registerDto.getEmail());
+        return registerMapper.toResource(user);
     }
 
-    public LoginResource login(LoginDto loginDto){
-        User user=repository.findByEmailAndPassword(loginDto.getEmail(),loginDto.getPassword());
-        if(user == null){
+    public LoginResource login(LoginDto loginDto) {
+        User user = repository.findByEmailAndPassword(loginDto.getEmail(), loginDto.getPassword());
+        if (user == null) {
             throw new RuntimeException(ErrorConstants.WRONG_EMAIL_OR_PASSWORD);
         }
-        String token =jwtGenerator.generateToken(user.getId());
-        LoginResource resource=loginMapper.toResource(user);
+        String token = jwtGenerator.generateToken(user.getId());
+        LoginResource resource = loginMapper.toResource(user);
         resource.setToken(token);
         return resource;
+    }
+
+    public void sendActivationToken(String email) {
+        String confirmationToken = jwtGenerator.generateTokenByEmail(email);
+        SimpleMailMessage mailMessage = createMail(email);
+        mailMessage.setTo(email);
+        mailMessage.setSubject(CONFIRM_ACCOUNT_HEADER);
+        mailMessage.setText(CONFIRM_ACCOUNT_BODY
+                + this.environment.getProperty("spring.url")
+                + CONFIRM_ACCOUNT_URL + confirmationToken);
+        sendMail(mailMessage);
+    }
+
+    public void sendResetPasswordsToken(String email) {
+        String confirmationToken = jwtGenerator.generateTokenByEmail(email);
+        SimpleMailMessage mailMessage = createMail(email);
+        mailMessage.setSubject(RESET_PASSWORD_HEADER);
+        mailMessage.setText(RESET_PASSWORD_BODY
+                + this.environment.getProperty("spring.url")
+                + RESET_PASSWORD_URL + confirmationToken);
+        sendMail(mailMessage);
+    }
+
+
+    public SimpleMailMessage createMail(String email) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(email);
+        return mailMessage;
+    }
+
+    public void sendMail(SimpleMailMessage mailMessage) {
+        try {
+            javaMailSender.send(mailMessage);
+        } catch (MailException exception) {
+            throw new RuntimeException(ErrorConstants.MAIL_SEND_FAILED);
+        }
+    }
+
+    @Modifying
+    @Transactional
+    public void confirmRegister(String token) {
+        UUID id = jwtResolver.getIdFromToken(token);
+        User user = repository.getOne(id);
+        if (user==null){
+            throw new RuntimeException(USER_NOT_EXIST);
+        }
+        user.setConfirmed(true);
+        repository.save(user);
+    }
+
+
+    public UserResource getUserByToken(String token) {
+        UUID id = jwtResolver.getIdFromToken(token);
+        User user = repository.getOne(id);
+        if (user==null){
+            throw new RuntimeException(USER_NOT_EXIST);
+        }
+        return registerMapper.toResource(user);
+    }
+
+    public void resetPassword(String token, String password) {
+        UUID id = jwtResolver.getIdFromToken(token);
+        User user = repository.getOne(id);
+        if (user==null){
+            throw new RuntimeException(USER_NOT_EXIST);
+        }
+        user.setPassword(password);
+        repository.save(user);
     }
 }
