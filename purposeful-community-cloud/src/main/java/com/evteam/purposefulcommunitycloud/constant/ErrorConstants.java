7116993390e0076c4e5f042667509c86efedb870
Emir Gökdemir <44062527+gokdeemir@users.noreply.github.com>
2019-12-18 22:01:20 +0300

    Field type method is added Error constants are added

--- purposeful-community-cloud/src/main/java/com/evteam/purposefulcommunitycloud/constant/ErrorConstants.java
+++ purposeful-community-cloud/src/main/java/com/evteam/purposefulcommunitycloud/constant/ErrorConstants.java
@@ -11,13 +11,15 @@ public class ErrorConstants {
     public static final String USER_NOT_EXIST = "User could not be found!";
     public static final String MAIL_SEND_FAILED = "Mail couldn't be sent!";
     public static final String WRONG_EMAIL_OR_PASSWORD = "The given email or password is wrong!";
-    public static final String ONLY_CREATOR_CAN_ADD_BUILDER = "Only creators can add builder!";
+    public static final String ONLY_CREATOR_CAN_EDIT_BUILDER = "Only creators can edit builder!";
     public static final String ONLY_CREATOR_CAN_SEE_BUILDER = "Only creators can see builders!";
     public static final String USER_ALREADY_FOLLOWED_THIS_COMMUNITY = "User already followed this community";
     public static final String COMMUNITY_IS_PRIVATE= "Community is private!";
     public static final String COMMUNITY_IMAGE_CAN_BE_CHANGED_BY_CREATOR= "Community image can be change by creator only!";
     public static final String THERE_IS_NO_COMMUNITY_WITH_THIS_ID= "There is no community with this id!";
     public static final String THERE_IS_NO_IMAGE_WITH_THIS_ID= "There is no image with this id!";
+    public static final String REQUIRED_FIELDS_CANNOT_BE_BLANK= "Required fields cannot be blank!";
+    public static final String YOU_CAN_NOT_DELETE_THIS_DATA_INSTANCE= "You can not delete this data instance!";
 
 
 
