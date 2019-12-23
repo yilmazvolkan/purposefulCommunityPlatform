e997fb4a986613cd2d3636347847962b2309a923
Volkan Yılmaz <volkan.yilmazboun@gmail.com>
2019-12-23 09:45:59 +0300

    Create instance initialisation

--- PurposefulCommunity/app/src/main/java/com/bounswe/purposefulcommunity/Adapters/TemplatesAdapter.kt
+++ PurposefulCommunity/app/src/main/java/com/bounswe/purposefulcommunity/Adapters/TemplatesAdapter.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bounswe.purposefulcommunity.Fragments.Templates.ShowTemplateActivity
+import com.bounswe.purposefulcommunity.Fragments.Instances.ShowTemplateActivity
 import com.bounswe.purposefulcommunity.Models.ShowTempBody
 import com.bounswe.purposefulcommunity.R
 import kotlinx.android.synthetic.main.item_temp.view.*
--- PurposefulCommunity/app/src/main/java/com/bounswe/purposefulcommunity/Fragments/Instances/CreateInstanceActivity.kt
+++ PurposefulCommunity/app/src/main/java/com/bounswe/purposefulcommunity/Fragments/Instances/CreateInstanceActivity.kt
@@ -1,9 +1,7 @@
-package com.bounswe.purposefulcommunity.Fragments.Templates
+package com.bounswe.purposefulcommunity.Fragments.Instances
 
 import android.content.Context
 import android.os.Bundle
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,35 +12,26 @@ import com.bounswe.purposefulcommunity.Adapters.FieldsAdapter
 import com.bounswe.purposefulcommunity.Models.AddTempBody
 import com.bounswe.purposefulcommunity.Models.GetFieldsBody
 import com.bounswe.purposefulcommunity.R
-import kotlinx.android.synthetic.main.activity_explore.*
-import kotlinx.android.synthetic.main.activity_show_template.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.net.ConnectException
 
-class ShowTemplateActivity : AppCompatActivity() {
-    private var hasHeader: Boolean = false
+class CreateInstanceActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_show_template)
+        setContentView(R.layout.activity_create_instance)
 
         val tempID = intent.getStringExtra("temp_id")
         val tempName = intent.getStringExtra("temp_name")
 
         val actionBar = supportActionBar
-        actionBar!!.title = tempName
+        actionBar!!.title = "Post: $tempName"
         actionBar.setDisplayHomeAsUpEnabled(true)
 
-        if(tempID!!.isNotEmpty()){
-            getFields(tempID)
-        }
-
-        fabAddInst.setOnClickListener {
-
-        }
     }
+
     private fun getFields(id: String){
         val res = getSharedPreferences("TOKEN_INFO", Context.MODE_PRIVATE)
         val tokenV = res.getString("token", "Data Not Found!")
@@ -52,14 +41,14 @@ class ShowTemplateActivity : AppCompatActivity() {
             override fun onFailure(call: Call<List<GetFieldsBody>>, t: Throwable) {
                 if(t.cause is ConnectException){
                     Toast.makeText(
-                        this@ShowTemplateActivity,
+                        this@CreateInstanceActivity,
                         "Check your connection!",
                         Toast.LENGTH_SHORT
                     ).show()
                 }
                 else{
                     Toast.makeText(
-                        this@ShowTemplateActivity,
+                        this@CreateInstanceActivity,
                         "Something bad happened!",
                         Toast.LENGTH_SHORT
                     ).show()
@@ -68,11 +57,11 @@ class ShowTemplateActivity : AppCompatActivity() {
             override fun onResponse(call: Call<List<GetFieldsBody>>, response: Response<List<GetFieldsBody>>) {
                 if (response.code() == 200) {
                     val rv = findViewById<RecyclerView>(R.id.recyclerViewFields)
-                    rv.layoutManager = LinearLayoutManager(this@ShowTemplateActivity, RecyclerView.VERTICAL, false)
+                    rv.layoutManager = LinearLayoutManager(this@CreateInstanceActivity, RecyclerView.VERTICAL, false)
 
                     val fields = ArrayList<AddTempBody>()
 
-                    var adapter = FieldsAdapter(this@ShowTemplateActivity, fields)
+                    var adapter = FieldsAdapter(this@CreateInstanceActivity, fields)
                     rv.adapter = adapter
 
                     val res: List<GetFieldsBody>? = response.body()
@@ -81,39 +70,15 @@ class ShowTemplateActivity : AppCompatActivity() {
                         fields.add(AddTempBody(i.fieldType, i.isRequired, i.name))
                     }
                     if(fields.isEmpty()){
-                        Toast.makeText(this@ShowTemplateActivity, "No field is found!", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this@CreateInstanceActivity, "No field is found!", Toast.LENGTH_SHORT).show()
                     }
                     //runLayoutAnimation()
                     adapter.notifyDataSetChanged()
 
                 } else {
-                    Toast.makeText(this@ShowTemplateActivity, "Your fields cannot retrieve!", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@CreateInstanceActivity, "Your fields cannot retrieve!", Toast.LENGTH_SHORT).show()
                 }
             }
         })
-    }
-    private fun runLayoutAnimation() = recyclerView.apply {
-        layoutAnimation = AnimationUtils.loadLayoutAnimation(context, R.anim.layout_animation_fall_down)
-        adapter?.notifyDataSetChanged()
-        scheduleLayoutAnimation()
-
-        if (hasHeader) {
-            layoutAnimationListener = object : Animation.AnimationListener {
-                override fun onAnimationStart(animation: Animation?) {
-                    layoutManager?.findViewByPosition(0)?.clearAnimation()
-                }
-                override fun onAnimationEnd(animation: Animation?) = Unit
-                override fun onAnimationRepeat(animation: Animation?) = Unit
-            }
-        }
-    }
-    override fun onSupportNavigateUp(): Boolean {
-        finish()
-        onBackPressed()
-        return true
-    }
-    override fun finish() {
-        super.finish()
-        overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
     }
 }
