0ef228e0e2ecc3273706113c9601328cd84d8c22
Volkan Yılmaz <volkan.yilmazboun@gmail.com>
2019-12-30 13:22:25 +0300

    Create template of templates is implemented

--- PurposefulCommunity/app/src/main/java/com/bounswe/purposefulcommunity/Models/CreateTemplateBody.kt
+++ PurposefulCommunity/app/src/main/java/com/bounswe/purposefulcommunity/Models/CreateTemplateBody.kt
@@ -1,8 +1,11 @@
 package com.bounswe.purposefulcommunity.Models
 
+import com.google.gson.JsonObject
+
 data class CreateTemplateBody(
     val communityId: String,
     val fields: List<AddTempBody>,
     val name: String,
-    val userId: String
+    val userId: String,
+    val templatesNameTemplateId: JsonObject
 )
